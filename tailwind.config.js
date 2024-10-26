6efff857656e82be1122bef94d902a2ad3d50325
yuexin-z11 <teresayuexinzhang@gmail.com>
2024-10-26 13:42:32 -0500

    under construction

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
+  content: ["/home/y_z_spam/yuexin.z.github.io/src/**/*.{html,js}"],
+  theme: {
+    extend: {}
+  },
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+  daisyui: {
+    themes: ["bumblebee", "dark", "cyberpunk"]
   }
+};
